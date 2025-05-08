ba7c1a3801332d27b756fef8b5628fe9e90b2882
MiracleMark35 <markianfrost123@gmail.com>
2025-05-08 10:28:44 +0800

    Library Import for Work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
 
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation(libs.androidx.work.runtime)
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
     implementation ("androidx.work:work-runtime:2.9.0")
