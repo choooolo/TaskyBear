6ca51269583d2e2e418afc67d1de5d74c59aad3d
erunzd <23103949@usc.edu.ph>
2025-05-14 01:51:50 +0800

    home profile fix
    maintain circle profile with uploaded photo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
       implementation ("com.github.yalantis:ucrop:2.2.8")
     implementation ("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation ("com.google.android.material:material:1.11.0")
 
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
